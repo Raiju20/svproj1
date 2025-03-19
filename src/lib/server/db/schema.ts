17869d224a4f4524e8a3e27e3b4369503f37e6e4
Raiju20 <ivanberestocoy9@gmail.com>
2025-03-19 13:29:14 +0500

    tailwind

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -15,6 +15,22 @@ export const session = sqliteTable('session', {
 	expiresAt: integer('expires_at', { mode: 'timestamp' }).notNull()
 });
 
+export const albums = sqliteTable("albums", {
+	albumId: integer("AlbumId").primaryKey({ autoIncrement: true }).notNull(),
+	title: text("Title", { length: 160 }).notNull(),
+	artistId: integer("ArtistId").notNull().references(() => artists.artistId),
+});
+
+export const artists = sqliteTable("artists", {
+	artistId: integer("ArtistId").primaryKey({ autoIncrement: true }).notNull(),
+	name: text("Name", { length: 120 }),
+	country: text("Country", { length: 120 }),
+});
+
 export type Session = typeof session.$inferSelect;
 
 export type User = typeof user.$inferSelect;
+
+export type Artist = typeof artists.$inferSelect;
+
+export type Album = typeof albums.$inferSelect;
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script>
-    let { children } = $props();
+	import '../app.css';
+	let { children } = $props();
 </script>
-  
+
 <nav>
-    <a href="/">home</a>
-    <a href="/demo/lucia/login">lucia</a>
+	<a href="/" class="focus:text-cyan-700">home</a>
+	<a href="/demo/lucia/login" class="focus:text-cyan-700">lucia</a>
 </nav>
 
 {@render children()}
